5769d244da397fa76496720fa8e47896e8f23a2d
ysuolmai <fan0417@gmail.com>
2025-02-24 13:47:12 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -97,7 +97,7 @@ if [[ $WRT_SOURCE == *"lede"* ]]; then
 fi
 
 #补齐依赖
-sudo -E apt-get -y install $(curl -fsSL is.gd/depends_ubuntu_2204)
+sudo -E apt-get -y install $(curl -fsSL https://raw.githubusercontent.com/ophub/amlogic-s9xxx-armbian/main/compile-kernel/tools/script/ubuntu2204-make-openwrt-depends)
 
 keywords_to_delete=(
     "xiaomi_ax3600" "xiaomi_ax9000" "xiaomi_ax1800" "glinet" "jdcloud_ax6600" "kucat" "re-cs-02"
